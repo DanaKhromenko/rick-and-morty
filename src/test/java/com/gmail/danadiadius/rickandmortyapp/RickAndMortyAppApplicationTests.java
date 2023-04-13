02a2f2de3a92a6c1cd07ee3cc84ed3151b1a00eb
DanaKhromenko <danadiadius@gmail.com>
2023-04-12 21:34:16 -0600

    Added init test

---
+++ src/test/java/com/gmail/danadiadius/rickandmortyapp/RickAndMortyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.danadiadius.rickandmortyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
